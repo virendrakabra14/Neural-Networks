1670559ce5436137276bbafb604cf1277a58322b
Virendra Kabra <virendrakabra14@gmail.com>
2021-12-16 15:53:53 +0530

    files added

---
+++ Neural-Network/functions.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+def sigmoid(Z):
+    return 1/(1+np.exp(-Z)), Z
+
+def relu(Z):
+    return np.maximum(0,Z), Z
+
+def relu_backward(dA, cache):
+
+    dZ = np.array(dA, copy=True)
+    dZ[cache <= 0] = 0          # if Z=cache<=0, set dZ=0
+
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    
+    sigmoid = 1/(1+np.exp(-cache))            # cached Z, now used
+    dZ = dA * sigmoid * (1-sigmoid)
+    
+    return dZ
+
+
+def initialize_parameters(n_x, n_h, n_y):
+
+    np.random.seed(1)
+    
+    W1 = np.random.randn(n_h, n_x)*0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h)*0.01
+    b2 = np.zeros((n_y, 1))
+    
+    return {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+
+def initialize_parameters_deep(layer_dims):
+    
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1]) #*0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A, W, b):
+    return W.dot(A)+b, (A, W, b)
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+    Z, linear_cache = linear_forward(A_prev, W, b)
+
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    return A, (linear_cache, activation_cache)
+
+def L_model_forward(X, parameters):
+
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    
+    for l in range(1, L):                   # for L-1 layers, ReLU is used as activation function
+        A_prev = A 
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], activation = "relu")
+        caches.append(cache)
+    
+    # for last layer, sigmoid activation used
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation = "sigmoid")
+    caches.append(cache)
+    
+    return AL, caches
+
+def compute_cost(AL, Y):
+    cost = (1./Y.shape[1]) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
+    return np.squeeze(cost)
+
+def linear_backward(dZ, cache):
+
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1./m * np.dot(dZ,A_prev.T)
+    db = 1./m * np.sum(dZ, axis = 1, keepdims = True)
+    dA_prev = np.dot(W.T,dZ)
+    
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+
+    linear_cache, activation_cache = cache
+    
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    
+    return dA_prev, dW, db
+
+def L_model_backward(AL, Y, caches):
+
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+    
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    
+    current_cache = caches[L-1]
+    grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, "sigmoid")
+    
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache, activation = "relu")
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+def update_parameters(parameters, grads, learning_rate):
+
+    L = len(parameters) // 2
+
+    for l in range(L):
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * grads["dW" + str(l+1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l+1)]
+        
+    return parameters
+
+def predict(X, y, parameters):
+
+    n = len(parameters) // 2
+    p = np.zeros((1,X.shape[1]))
+    
+    probas, caches = L_model_forward(X, parameters)
+    
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    
+    print("Accuracy: "  + str(np.sum((p == y)/X.shape[1])))
+    return p
